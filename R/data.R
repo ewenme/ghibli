f39da7e45c0584a2a7c2b4557fda875b8ec3f4a0
ewen_ <ewenhenderson@gmail.com>
2020-04-16 08:23:33 +0100

    fix url

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A list of all colour palettes to choose from, and preview directly in the console.
 #'
-#' Pretty print and plot methods are powered by the \href{https://cran.r-project.org/web/packages/prismatic/index.html}{prismatic}
+#' Pretty print and plot methods are powered by the \href{https://CRAN.R-project.org/package=prismatic}{prismatic}
 #' package. Each palette is stored as a \code{color} object to enact this behaviour.
 #'
 #' @seealso See \code{\link{ghibli_palette}} to construct palettes of desired length, and
